3902c1d79d392e04481f4054c0a5b34a50b02e1c
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-03-19 15:20:42 +0000

    Bootable

--- build.bat
+++ build.bat
@@ -6,3 +6,5 @@ cmake --build build --target demo_ecs --config Release || exit /b 1
 cmake --build build --target demo_aga --config Release || exit /b 1
 echo Built build\Release\demo_ecs
 echo Built build\Release\demo_aga
+copy /Y build\Release\demo_ecs boot\demo_ecs
+copy /Y build\Release\demo_aga boot\demo_aga
